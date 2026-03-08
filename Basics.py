ccb377e60b222118146cf3c5b4ad13590e7c9796
himanshu sharma <sharmapriyanshu05247@gmail.com>
2026-03-08 20:21:44 +0530

    Basics

---
+++ Basics.py
@@ -0,0 +1,146 @@
+1.Write a program to print “Hello Python” on the screen.
+a="Hello Python"
+print (a)
+
+2.Take a name as input and print 
+Welcome <name> 
+name = input("Enter a name:")
+print("welcome",name)
+
+
+3.Take a number as input and print it. 
+num = int(input("Write a number:"))
+print(num)          
+
+
+4.Take two numbers as input and print both numbers on separate lines. 
+num1= int(input("1:"))
+num2= int(input("2:"))
+print(num1)
+print(num2)
+
+
+
+5.Take a name and age as input and print: 
+My name is ___ and my age is ___ 
+N = input("Name:")
+A = input("Age:")
+print("My name is",N,"and my age is",A)
+
+
+
+6.Take an integer as input and print its square
+num = int(input("Enter a number:"))
+sqr = num*num
+print("Square:",sqr)
+
+
+
+7.Take two integers as input and print their sum.
+A= int(input("Enter A number:"))
+B= int(input("Enter B number:"))
+C= A+B
+print("Sum of two numbers:",C)
+
+
+
+8.Take two integers and print: 
+o Addition 
+o Subtraction 
+o Multiplication 
+print(34+7)
+print(34-7)
+print(34*7)
+
+
+
+9.Take a number and print: 
+The number entered is <number> 
+a=34
+print("The number entered is",a)
+
+
+10. Take an integer and print its double. 
+num=45
+print("Double:",num*2)
+
+
+
+11.Take a float value as input and print it.
+V=float(input("Enter a value:"))
+print(V)
+
+
+
+
+12.Take two float numbers and print their sum.
+a=34.54
+b=56.64
+c=a+b
+print(c)
+
+
+
+
+13.Take a float number and print its half value.
+a=456.78
+print(a/2)
+
+
+
+
+14.Take length and width (float) as input and
+print the area of a rectangle.  (length*width)
+
+Len=float(input("Enter Length:"))
+Wid=float(input("Enter Width:"))
+Area=Len*Wid
+print("Area of rectangle:",Area)
+          
+
+
+
+15.Take radius as float input and print the area of a circle 
+(Use π = 3.14) 
+r=float(input("Enter Radius:"))
+area=3.14*r*r
+print("Area of circle:",area)
+
+
+
+
+16.Take one integer and one float as input and print both. 
+a=float(input("A:"))
+b=int(input("B:"))
+print(a,b)
+
+
+
+17.Take total marks (int) and number of subjects (int) and
+print the average as float.
+
+TM=575
+NOS=7
+print("Average",float(TM/NOS))
+
+
+
+18.Take price (float) and quantity (int) and print the total cost. 
+P=float(499.50)
+Q=int(6)
+print("Total cost:",P*Q)
+
+
+
+
+19.Take two numbers (one int, one float) and print their sum. 
+a=int(45)
+b=float(34.55)
+print("Sum:",a+b)
+
+
+
+20.Take salary (float) and bonus (int) and print the final salary.
+Salary=float(56778.75)
+Bonus=int(1499)
+print("Final Salary:",Salary+Bonus)
